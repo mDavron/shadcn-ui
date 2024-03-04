acbb620a93aec4786e59dd94423468106e97d53f
Davron01~ <d.mirabdullayev@mail.ru>
2024-03-04 14:40:01 +0500

    added card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,54 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Badge } from "@/components/ui/badge"
+import { Button } from "@/components/ui/button"
 
-export default function Home() {
+interface Recipe {
+  title: string,
+  image: string,
+  time: number,
+  description: string,
+  vegan: boolean,
+  id: string
+}
+async function getRecipes(): Promise<Recipe[]> {
+  const result = await fetch('http://localhost:4000/recipes')
+  return result.json()
+
+}
+export default async function Home() {
+  const recipes = await getRecipes();
   return (
-    <main>Home</main>
+    <main className="px-2">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+        {recipes.map(recipe => (
+          <Card key={recipe.id} className="flex flex-col justify-between">
+            <CardHeader className=" flex flex-row gap-4 items-center">
+              <Avatar>
+                <AvatarImage src={`/img/${recipe.image}`} alt={recipe.image} />
+                <AvatarFallback>{recipe.title.slice(0, 2)}</AvatarFallback>
+              </Avatar>
+              <div>
+                <CardTitle>{recipe.title}</CardTitle>
+                <CardDescription>{recipe.time} mins to cook.</CardDescription>
+              </div>
+            </CardHeader>
+            <CardContent><p>{recipe.description}</p></CardContent>
+            <CardFooter className="flex justify-between">
+              <Button>View Recipe</Button>
+              {recipe.vegan && <Badge variant="secondary">Vegan!</Badge>}
+            </CardFooter>
+
+          </Card>
+        ))}
+      </div>
+    </main>
   );
 }
